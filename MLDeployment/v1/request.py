b3f13bc516f10e5dbeef59311a2defaf2f5b1295
Shreyansh Singh <shreyansh.pettswood@gmail.com>
2020-09-27 02:04:41 +0530

    Add request test file

---
+++ MLDeployment/v1/request.py
@@ -0,0 +1,12 @@
+import requests
+
+to_predict_dict = {
+    "data": [
+        [4.8, 3, 1.4, 0.3],
+        [2, 1, 3.2, 1.1]
+    ]
+}
+
+url = 'http://127.0.0.1:8000/api'
+r = requests.post(url, json=to_predict_dict);
+print(r.json())
