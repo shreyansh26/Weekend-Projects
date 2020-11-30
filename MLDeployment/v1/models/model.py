beff51182f0e6a9d0040cb62adada465fb0a6aba
Shreyansh Singh <shreyansh.pettswood@gmail.com>
2020-11-30 17:12:57 +0530

    Fix typo

--- MLDeployment/v1/models/model.py
+++ MLDeployment/v1/models/model.py
@@ -10,7 +10,7 @@ ada = AdaBoostClassifier(n_estimators=5)
 X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.1)
 model = ada.fit(X_train, y_train)
 
-print("Mode score: ", ada.score(X_train, y_train))
+print("Model score: ", ada.score(X_train, y_train))
 print("Test Accuracy: ", ada.score(X_test, y_test))
 
 pickle.dump(model, open("model.pkl", "wb"))
